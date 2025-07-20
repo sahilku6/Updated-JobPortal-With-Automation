9054ba67043c14be3a6e415c19ddf7392b5e080a
Sahil Kumar <sahilkumarlpu1@gmail.com>
2025-07-20 15:10:47 +0530

    add cloudinary depen etc

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
-import dotenv from "dotenv";
 import app from "./app.js";
+import cloudinary from "cloudinary";
 
-dotenv.config({ path: "./config/config.env" });
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server listening at port ${process.env.PORT}`);
